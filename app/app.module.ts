07c027987a2567d0df61fc939889d242ab08fa59
Tomáš Oplatek <tomas.oplatek@xpis.cz>
2016-08-26 18:03:46 +0200

    1st part of chapter 6 done

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
-
+import { HeroesComponent } from './heroes.component';
 import { AppComponent }  from './app.component';
+import { HeroService } from './hero.service';
+import { routing } from './app.routing'
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [ BrowserModule, FormsModule, routing ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent ],
+  bootstrap: [ AppComponent ],
+  providers: [HeroService]
 })
 export class AppModule { }
